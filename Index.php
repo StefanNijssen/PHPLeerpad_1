0d642b56fc9d133ba4d6e0e90ca6a55a3f402dcb
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2023-05-05 13:13:24 +0200

    af

--- Index.php
+++ Index.php
@@ -16,16 +16,30 @@ $current_hour = date('H', $current_time); // haal het huidige uur op in 24-uurs 
 echo $current_hour ;
 // controleer of het tussen 6.00 en 12.00 uur is
 if ($current_hour >= 0 && $current_hour < 6) {
+	$background_image = 'img/night.png';
     echo "Het is momenteel tussen 00.00 en 6.00 uur.";
 } else if ($current_hour >= 6 && $current_hour < 12) {
+	$background_image = 'img/morning.png';
     echo "Het is momenteel tussen 6.00 en 12.00 uur.";
 } else if ($current_hour >= 12 && $current_hour < 18) {
+	$background_image = 'img/afternoon.png';
     echo "Het is momenteel tussen 12.00 en 18.00 uur.";
 } else 
+	$background_image = 'img/evening.png';
     echo "Het is momenteel tussen 18.00 en 00.00 uur.";
 
 echo "test";
+
+echo '<html>';
+echo '<head>';
+echo '<style>';
+echo 'body {';
+echo '    background-image: url("' . $background_image . '");'; // apply the background image
+echo '    background-size: cover;'; // adjust the background size
+echo '    background-repeat: no-repeat;'; // prevent background image repetition
+echo '}';
+echo '</style>';
+echo '</head>';
+
+echo '</html>';
 ?>
-<div>
-    test
-</div>
